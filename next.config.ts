71588696a187b8fee7c452a4310b52d8e2ce895f
Raheel Arfeen <raheelarfeen@gmail.com>
2025-07-18 18:22:54 +0600

    update done

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  // Add the transpilePackages property
-  transpilePackages: ['@react-spring/web'],
 };
 
 export default nextConfig;
